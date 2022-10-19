084450b939c6b5b7344c32cc1f1f79401c79b55f
jay3490 <jaysorathiya0421@gmail.com>
2022-10-19 14:58:45 +0530

    Inital commit

---
+++ API/DTO/RegisterDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,31 @@
+﻿using API.Extentions;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+        public string KnowAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateofBirth.CalculateAge();
+        }
+    }
+}
